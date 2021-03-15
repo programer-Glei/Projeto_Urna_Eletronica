939adad876f1fa2d1940f382e2a1c12a728a4915
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-03-15 18:36:31 -0300

    proximo comit

---
+++ javascript/etapas.js
@@ -0,0 +1,52 @@
+
+let etaps = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'3811.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'cic',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-prefeito',small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'DRFT',
+                vice: 'cic',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Vereador'},
+                    {url:'84_2.jpg', legenda: 'Vice-prefeito',small: true}
+                ]
+            }
+        ]
+    },
+]
